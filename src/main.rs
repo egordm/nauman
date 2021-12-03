06da1de7c1360062440078cfee95e5be6e44789a
Egor Dmitriev <egordmitriev2@gmail.com>
2021-12-04 00:34:41 +0100

    [FEATURE] Added main

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::{
+    fs,
+    io,
+    fs::File,
+};
+use crate::execution::execute_flow;
+
+mod common;
+mod config;
+mod output;
+mod flow;
+mod execution;
+
+fn main() {
+    let contents = fs::read_to_string("example/test.yml")
+        .expect("Something went wrong reading the file");
+
+    let job: config::Job = serde_yaml::from_str(&contents).unwrap();
+    let flow = flow::Flow::from_job(&job).unwrap();
+
+    execute_flow(&flow).unwrap();
+}
